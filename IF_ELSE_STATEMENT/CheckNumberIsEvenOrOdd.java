c4e52ed85f5a3f15092bc44bbaa122d8aebba732
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-07 01:04:46 +0530

    Create CheckNumberIsEvenOrOdd.java

---
+++ IF_ELSE_STATEMENT/CheckNumberIsEvenOrOdd.java
@@ -0,0 +1,10 @@
+public class CheckNumberIsEvenOrOdd {
+    public static void main(String[] args) {
+        int num = 4;
+        if(num % 2 == 0){
+            System.out.println("Number is even.");
+        }else{
+            System.out.println("Number is odd.");
+        }
+    }
+}
