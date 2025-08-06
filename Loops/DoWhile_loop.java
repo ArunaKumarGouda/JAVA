377834095683f4cdd7556545ef31425a7d3e5cf0
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-06 19:06:08 +0530

    Create DoWhile_loop.java

---
+++ Loops/DoWhile_loop.java
@@ -0,0 +1,10 @@
+public class DoWhile_loop {
+    public static void main(String[] args){
+
+        int a = 5;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a <= 10);
+    } 
+}
