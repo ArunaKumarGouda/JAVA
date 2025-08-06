0cae5963684c69943a94129616e1121586f61152
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-07 00:46:57 +0530

    Create Break_statement.java

---
+++ Break_and_Continue_Statement/Break_statement.java
@@ -0,0 +1,10 @@
+public class Break_statement {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 30; i++){
+            System.out.println(i);
+            if(i == 15){
+                break;
+            }
+        }
+    }
+}
