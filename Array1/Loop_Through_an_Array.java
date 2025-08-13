e31d9494b79af3eb1c97a47e362403d9167291b2
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-13 18:12:10 +0530

    Create Loop_Through_an_Array.java

---
+++ Array1/Loop_Through_an_Array.java
@@ -0,0 +1,8 @@
+class Btech { 
+public static void main(String[] args) { 
+String[] cars = {"Volvo", "BMW", "Ford", "Jaguar"}; 
+for (int i = 0; i < cars.length; i++) { 
+System.out.println(cars[i]); 
+} 
+} 
+}
