b661d3af862805cf625fe1b714a3d0e1bf2aeb46
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-21 22:11:04 +0530

    Create User_input_Array.java

---
+++ Array1/User_input_Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UserInputArray {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    
+    System.out.println("Enter size of array :");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    
+    System.out.println("Enter " + n + " elements :");
+    for (int i = 0; i < arr.length; i++) {
+        arr[i] = sc.nextInt();
+        }
+        System.out.println("The array elements are :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
