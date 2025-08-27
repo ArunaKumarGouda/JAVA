a323f6348dda4e520f6204f660c8e9b4ca18d846
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-27 23:19:40 +0530

    Create Object.java

---
+++ For Practice Java/Object.java
@@ -0,0 +1,20 @@
+class Car{
+    int id;
+    String name;
+    float number;
+}
+
+public class Call_by_Value1 {
+ 
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.id = 19;
+        c.name = "Aruna";
+        c.number = 2.6f;
+
+        System.out.println(c.id);
+        System.out.println(c.name);
+        System.out.println(c.number);
+
+    }
+ }
