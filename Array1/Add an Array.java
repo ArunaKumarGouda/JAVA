07cd67260fc7825939f6dc5730af41cd29dbb3f9
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-08-28 19:41:17 +0530

    Create Add an Array.java

---
+++ Array1/Add an Array.java
@@ -0,0 +1,17 @@
+public class Arrayproblems {
+
+   static void addAnArray(){
+
+        int[] arr = {4, 5, 7, 9, 2};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("The sum of array elements are :" + sum);
+    }
+  public static void main(String[] args) {
+        addAnArray();
+  }
+}
