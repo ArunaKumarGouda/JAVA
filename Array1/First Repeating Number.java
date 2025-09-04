e2b40fab83aaad4f3ba927b7e022dd6f41e8076e
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-09-04 17:20:23 +0530

    Create First Repeating Number.java

---
+++ Array1/First Repeating Number.java
@@ -0,0 +1,37 @@
+/*  Q : Given an array 'a' consisting of integer.
+        Return the first value that is repeating in this array.
+        If no value is being repeated, return -1.  */
+                   
+package Array;
+import java.util.Scanner;
+
+public class RepeatingNumber {
+
+    static int firstRepeatingNumber(int[] arr){
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            for ( int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]){
+                   return arr[i];
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size :");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements :");
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The first repeating number is :" + firstRepeatingNumber(arr));
+        sc.close();
+        
+    }
+}
