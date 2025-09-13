c20ae4bb88fed6dce7c4090951ae44e99e549c83
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-09-13 22:35:33 +0530

    Create Sort 0s, 1s and 2s.java

---
+++ Sorting/Sort 0s, 1s and 2s.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int low = 0, mid = 0, high = arr.length - 1;
+        
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                // Swap arr[low] and arr[mid]
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++; // leave 1 in the middle
+            } else { // arr[mid] == 2
+                // Swap arr[mid] and arr[high]
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                
+                high--;
+            }
+        }
+    }
+}
