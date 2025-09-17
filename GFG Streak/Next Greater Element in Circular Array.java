0d2c7f3b9fcca29166b777085c1e69bee26d0b73
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-09-18 00:06:04 +0530

    Create Next Greater Element in Circular Array.java

---
+++ GFG Streak/Next Greater Element in Circular Array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> nextGreater(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>(Collections.nCopies(n, -1));
+        Stack<Integer> st = new Stack<>();
+
+        // Traverse twice (simulate circular array)
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int idx = i % n;
+            while (!st.isEmpty() && arr[st.peek()] <= arr[idx]) {
+                st.pop();
+            }
+            if (!st.isEmpty()) {
+                res.set(idx, arr[st.peek()]);
+            }
+            st.push(idx);
+        }
+
+        return res;
+    }
+}
