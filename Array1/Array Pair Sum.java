cdc5b7fc8faceead5c7f1dda3325073caedcd741
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-09-29 09:32:13 +0530

    Update Array Pair Sum.java

--- Array1/Array Pair Sum.java
+++ Array1/Array Pair Sum.java
@@ -6,7 +6,7 @@ public class ArrayPairSum {
         for(int i = 0; i < n; i++){
             for (int j = i + 1; j < n; j++){
                 if (arr[i] + arr[j] == target){
-                    ans++;
+                    ans++; 
                 }
              }
         }
