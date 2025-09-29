621ce63d7e87f15f87499fefb5d6e130b6614d89
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-09-29 09:53:29 +0530

    Update While_loop.java

--- Loops/While_loop.java
+++ Loops/While_loop.java
@@ -2,7 +2,7 @@ public class While_loop {
 
     public static void main(String[] args) {
         int i = 1;
-        while ((i <= 5)) {
+        while (i <= 5) {
             System.out.println(i);
             i++;
         }
