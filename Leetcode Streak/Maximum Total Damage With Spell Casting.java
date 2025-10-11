5133d3a52194096c0d231c82018f909b8081b7bd
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-10-11 09:59:28 +0530

    Create Maximum Total Damage With Spell Casting.java

---
+++ Leetcode Streak/Maximum Total Damage With Spell Casting.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public long maximumTotalDamage(int[] power) {
+        Map<Integer, Long> map = new HashMap<>();
+        for (int p : power)
+            map.put(p, map.getOrDefault(p, 0L) + p);  // total damage for each power
+        
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        
+        int n = keys.size();
+        long[] dp = new long[n];
+        
+        dp[0] = map.get(keys.get(0));
+        
+        for (int i = 1; i < n; i++) {
+            long take = map.get(keys.get(i));
+            
+            // find previous non-conflicting spell
+            int j = i - 1;
+            while (j >= 0 && keys.get(j) >= keys.get(i) - 2) j--;
+            if (j >= 0) take += dp[j];
+            
+            long skip = dp[i - 1];
+            dp[i] = Math.max(skip, take);
+        }
+        
+        return dp[n - 1];
+    }
+}
