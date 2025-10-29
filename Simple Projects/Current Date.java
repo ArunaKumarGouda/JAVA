af0b4197cc615f7cee9218b6765808fc4188168e
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-10-29 12:08:55 +0530

    Create Current Date.java

---
+++ Simple Projects/Current Date.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class time {
+
+    public static void main(String[] args) {
+ 
+        LocalDate d = LocalDate.now();
+
+        System.out.println(d);
+    }
+}
