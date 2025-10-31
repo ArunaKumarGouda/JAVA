30db4786b411ce45326a8ca10cd0225b354a6cc0
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2025-10-31 09:21:12 +0530

    Add array rotation function in Java
    Implemented a function to rotate an array by d elements in a counter-clockwise direction.

---
+++ GFG1/Rotate an Array K Times.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int n = arr.length;
+        
+        d = d % n;
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = arr[(i + d) % n];
+        }   
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+}
