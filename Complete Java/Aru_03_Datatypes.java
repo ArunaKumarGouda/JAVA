266b025cff8f7d9b35b05f8101e91d4674c1d4d1
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-07-26 00:05:29 +0530

    Create Aru_03_Datatypes.java

---
+++ Complete Java/Aru_03_Datatypes.java
@@ -0,0 +1,21 @@
+public class Aru_03_Datatypes {
+    public static void main(String[] args) {
+        boolean flag = true;
+        int a = 2345;
+        short s = 1200;
+        byte b = 120;
+        long l = 234234456L;
+        float f = 354.642f;
+        double d = 23445.5634;
+        char c = 'a';
+
+        System.out.println("The boolean variable is : " + flag);
+        System.out.println("The int variable is : " + a);
+        System.out.println("The short variable is : " + s);
+        System.out.println("The byte variable is : " + b);
+        System.out.println("The long variable is : " + l);
+        System.out.println("The float variable is : " + f);
+        System.out.println("The double variable is : " + d);
+        System.out.println("The char variable is : " + c);
+    }
+}
