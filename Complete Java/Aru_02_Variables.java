6d94f2784a5e37b0dc996936da58d733260074bc
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-07-25 23:46:11 +0530

    Add Aru_02_Variables.java to demonstrate variables

---
+++ Complete Java/Aru_02_Variables.java
@@ -0,0 +1,11 @@
+// A variable is a named memory location used to store data.
+// The value stored in a variable can change during program execution.
+
+public class Aru_02_variables {
+    public static void main(String[] args) {
+        int money = 2000;
+        System.out.println("Old money : " + money);
+        money = 3000;
+        System.out.println("New money : " + money);
+    }
+}
