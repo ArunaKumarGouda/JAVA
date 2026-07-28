2415ddf571140616ed208783a7bdf16795baf46f
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-07-29 00:09:07 +0530

    Add arithmetic operations in Aru_09_Arithmetic_Operators
    Implement arithmetic operations with addition, subtraction, multiplication, division, and modulus.

---
+++ Complete Java/Aru_09_Arithmetic_Operators.java
@@ -0,0 +1,13 @@
+// Arithmetic operations are mathematical operations performed on numbers using operators such as +, -, *, /, and %.
+
+public class Aru_09_Arithmetic_Operators {
+    public static void main(String[] args) {
+        int p = 20, q = 9;
+
+        System.out.println("Add : " + (p + q));
+        System.out.println("sub : " + (p - q));
+        System.out.println("mul : " + (p * q));
+        System.out.println("div : " + (p / q));
+        System.out.println("mod : " + (p % q));
+    }
+}
