1baf76d11611bf245c889f2400bc2ff6101ee8fc
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-07-29 00:10:34 +0530

    Create Aru_10_Relational_Operators.java
    Added a Java program demonstrating relational operators.

---
+++ Complete Java/Aru_10_Relational_Operators.java
@@ -0,0 +1,15 @@
+// Relational operators are used to compare two values and return either true or false
+
+public class Aru_10_Relational_Operators {
+    public static void main(String[] args) {
+
+        int p = 10, q = 15;
+
+        System.out.println(p == q);
+        System.out.println(p != q);
+        System.out.println(p <= q);
+        System.out.println(p >= q);
+        System.out.println(p < q);
+        System.out.println(p > q);
+    }
+}
