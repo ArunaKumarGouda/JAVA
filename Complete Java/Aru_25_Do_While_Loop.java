b5da968c7406f0cd8f03586652daf937b7aa37b3
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-08-13 21:58:07 +0530

    Create Aru_25_Do_While_Loop.java

---
+++ Complete Java/Aru_25_Do_While_Loop.java
@@ -0,0 +1,12 @@
+// The do-while loop is an exit-controlled loop that execute the loop body at least once before checking the condition.
+
+public class Aru_25_Do_While_Loop {
+    public static void main(String[] args) {
+        int num = 1;
+        do {
+            System.out.println(num);
+            num++;
+        }
+        while (num <= 10);
+    }
+}
