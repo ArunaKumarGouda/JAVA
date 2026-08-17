1859af08129ab32a6bdbafd598e99c9b7294498c
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-08-17 08:12:00 +0530

    Create Aru_32_Sum_of_Series.java

---
+++ Complete Java/Aru_32_Sum_of_Series.java
@@ -0,0 +1,25 @@
+// Find the sum of the following series
+// s = 1 - 2 + 3 - 4 ... n
+
+import java.util.Scanner;
+public class Aru_32_Sum_of_Series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                ans -= i;
+            }
+            else {
+                ans += i;
+            }
+        }
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
