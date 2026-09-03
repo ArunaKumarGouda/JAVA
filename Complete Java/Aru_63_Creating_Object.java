0f7d1da7e0de2b0943b583ce88364b461a353420
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-04 01:01:29 +0530

    Create Aru_63_Creating_Object.java

---
+++ Complete Java/Aru_63_Creating_Object.java
@@ -0,0 +1,11 @@
+
+public class Aru_63_Creating_Object {
+    int age = 20;
+    String name = "Aruna";
+
+    public static void main(String[] args) {
+        Aru_63_Creating_Object a = new Aru_63_Creating_Object();
+        System.out.println(a.age);
+        System.out.println(a.name);
+    }
+}
