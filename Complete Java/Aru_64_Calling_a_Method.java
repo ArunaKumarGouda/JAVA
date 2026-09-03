2a9f58ecc70591fa8df55b5c8463312adde6cbcc
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-04 01:01:59 +0530

    Create Aru_64_Calling_a_Method.java

---
+++ Complete Java/Aru_64_Calling_a_Method.java
@@ -0,0 +1,8 @@
+public class Aru_64_Calling_a_Method {
+    static void aruna() {
+        System.out.println("Wel-Come");
+    }
+    public static void main(String[] args) {
+        aruna();
+    }
+}
