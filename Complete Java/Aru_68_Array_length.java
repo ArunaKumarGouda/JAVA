e30d84b066d3cbac97a1ff6510e01c7b2c0cf01f
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-05 13:22:21 +0530

    Add Aru_68_Array_length.java to demonstrate array usage

---
+++ Complete Java/Aru_68_Array_length.java
@@ -0,0 +1,8 @@
+public class Aru_68_Array_length {
+    public static void main(String[] args) {
+        int[] arr = {12, 4, 65, 78, 3};
+
+        System.out.println(arr[2]);
+        System.out.println(arr.length);
+    }
+}
